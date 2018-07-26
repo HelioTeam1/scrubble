870a27dca7222a2d0269eff71dcae19d7586eea2
Unknown <dillon.embreus@gmail.com>
2018-07-26 15:27:34 -0600

    Added Comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {Component, Fragment} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             term: '',
@@ -13,52 +12,58 @@ class App extends Component {
         };
     }
 
-        // state = {
-        //     term: '',
-        //     results: []
-        // };
+    // state = {
+    //     term: '',
+    //     results: []
+    // };
 
-/*
- It submits a get request to the Urban Dictionary API with the term we want to query.
- */
+    /*
+     It submits a get request to the Urban Dictionary API with the term we want to query.
+     */
 
     handleSubmit = (e) => {
+        //prevents the info being sent to it's default destination
         e.preventDefault();
-        const data = JSON.stringify({...this.state})
 
+        //turns the state into JSON
+        // const data = JSON.stringify({...this.state})
+
+        //checks if the term is an empty string and doesn't do the GET request if it is
+        if(this.state.term === ""){
+            return;
+        }
+
+        //a GET fetch request that pulls info from urban dictionary
         fetch(`https://api.urbandictionary.com/v0/define?term=${this.state.term}`)
             .then(results => results.json())
             .then(data => this.setState({results: data.list}))
-
     }
 
-
-
     render() {
-
+        //variable results that is set to the current state
         const {results} = this.state;
         return (
             <Fragment>
+                {/*form for the text field and submit button*/}
                 <form onSubmit={this.handleSubmit}>
+                    {/*input for the text field*/}
                     <input type='text' placeholder="Enter Term"
                            onChange={event => this.setState({term: event.target.value})}/>
+                    {/*input for the submit*/}
                     <input type='submit'/>
-
-
-
                 </form>
-
-                <ul>
-                {results.map(
-                    result =>
-                        <li key={result.defid}>
-                            {/*{result.definition}*/}
-                            <a href={result.permalink}>{result.definition}</a>
-                        </li>
-                )}
-                </ul>
+                {/*ordered list for the definition results*/}
+                <ol>
+                    {/*maps out the results to display only the definitions*/}
+                    {results.map(
+                        result =>
+                            <li key={result.defid}>
+                                {/*{result.definition}*/}
+                                <a href={result.permalink}>{result.definition}</a>
+                            </li>
+                    )}
+                </ol>
             </Fragment>
-
         )
     }
 }
