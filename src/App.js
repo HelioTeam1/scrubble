96acc49800297bdc41bd9e19f973bb67ecfccd9e
Jordon Peterson <jordonpropm@gmail.com>
2018-07-26 11:43:55 -0600

    Made Form Connect to API
    As a team we connected to the urban dictionary api

--- src/App.js
+++ src/App.js
@@ -11,13 +11,20 @@ class App extends Component {
         };
 
 
-    componentDidMount() {
+/*
+ It submits a get request to the Urban Dictionary API with the term we want to query.
+ */
 
-        fetch('https://api.urbandictionary.com/v0/define?term=war')
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const data = JSON.stringify({...this.state})
+
+        fetch(`https://api.urbandictionary.com/v0/define?term=${this.state.term}`)
             .then(results => results.json())
             .then(data => this.setState({results: data.list}))
-        // console.log(this.state)
+
     }
+
 
 
     render() {
@@ -25,12 +32,21 @@ class App extends Component {
         const {results} = this.state;
         return (
             <Fragment>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder="Enter Term"
+                           onChange={event => this.setState({term: event.target.value})}/>
+                    <input type='submit'/>
+
+
+
+                </form>
+
                 <ul>
                 {results.map(
                     result =>
                         <li key={result.defid}>
-                            {result.definition}
-                            {/*<a href={result.permalink}>{result.definition}</a>*/}
+                            {/*{result.definition}*/}
+                            <a href={result.permalink}>{result.definition}</a>
                         </li>
                 )}
                 </ul>
